085314b517f311165e7d48ca01f550d1bad11153
MASARUL ALAM SHAON <137428560+masarulalamshaon@users.noreply.github.com>
2024-10-26 15:19:49 +0600

    Update Program.cs
    add new

--- Program.cs
+++ Program.cs
@@ -70,3 +70,84 @@ public class FirstSelenium
     }
 
 }
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+using AventStack.ExtentReports;
+using CsvHelper;
+using Demo_Registration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Globalization;
+
+public class FirstSelenium
+    
+{
+    public static string DataCSVFile= System.IO.Directory.GetCurrentDirectory();
+    
+
+    static void Main()
+    {
+        var testDataList = ReadFile(DataCSVFile + "\\data\\data.csv");
+
+
+        IWebDriver driver = new ChromeDriver();
+        ExtentReports extentReports = new ExtentReports();
+
+
+        driver.Navigate().GoToUrl("https://practicetestautomation.com/practice-test-login/");
+        Console.WriteLine("Open Browser");
+
+        driver.Manage().Window.Maximize();
+        Console.WriteLine("Browser Maximize");
+
+        foreach (var Testdata in testDataList)
+        {
+            driver.FindElement(By.Id("username")).SendKeys(Testdata.username);
+            Console.WriteLine("Provide username");
+            
+
+            driver.FindElement(By.Id("password")).SendKeys(Testdata.password);
+            Console.WriteLine("Provide Password");
+            driver.FindElement(By.Id("submit")).Click();
+            Console.WriteLine("Hit Submit button");
+            try
+            {
+                driver.FindElement(By.CssSelector(".wp-block-button__link")).Click();
+                break;
+            }
+            catch
+            {
+                Console.WriteLine("Failed Login");
+            }
+
+
+
+        }
+        
+
+
+
+        driver.Quit();
+
+
+
+
+
+    }
+    static List<Testdata> ReadFile(String filepath)
+        {
+
+            using (var reader = new StreamReader(filepath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+
+                return new List<Testdata>(csv.GetRecords<Testdata>());
+
+            }
+        }
+
+
+ }
